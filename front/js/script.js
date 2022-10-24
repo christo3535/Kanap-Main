68082ff051ab189a99eb811a8346582324ff0560
christotchalakov <christo3535@hotmail.com>
2022-10-24 18:39:06 +0300

    ajout newProduct à appendChild

--- front/js/script.js
+++ front/js/script.js
@@ -5,27 +5,22 @@
                 fetch("http://localhost:3000/api/products")
                 .then(data => data.json())
                 .then(products =>{
-
-
-
         
             for(let product of products){
             console.log(product.name);
         //insertion des produits-page d'acceuil
-        const newProduct = document.createElement("a");
-        newProduct.setAttribute("href", `product.html?id=${product._id}`);
+        const newProduct = document.createElement('a');
+        newProduct.setAttribute("href",`product.html?id=${product._id}`);
         newProduct.innerHTML =`
         <article>
-              <img src="${product.imageURL}" alt="${product.altTxt}">
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
               <h3 class"productName">${product.name}</h3>
               <p class"productDesciption">${product.description}</p> 
         </article>`;
-        addArticle.appendChild
+        addArticle.appendChild(newProduct);
 
 
         }
-
-        
 
         })
 
